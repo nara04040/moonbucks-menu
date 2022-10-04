1bc194d84232b0c445b1db3a791a4d37bdc6b01d
nara04040 <nara04040@naver.com>
2022-10-04 17:57:07 +0900

    챕터 2 localStrage에 수정하면 값이 저장 안되는 오류

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-// step1 요구사항 구현을 위한 전략
+/**
+ * // step1 요구사항 구현을 위한 전략
 // TODO메뉴 추가
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
 // - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
@@ -16,9 +17,48 @@
 // - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
 // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 
+ */
+
+// step2 요구사항
+
+// TODO localStorage Read & Wright
+// - [] localStorage에 데이터를 저장한다
+//  - [x] 메뉴를 추가할 때
+//  - [] 메뉴를 수정할 때
+//  - [] 메뉴를 삭제할 때
+// - [ ] localStorage에 있는 데이터를 읽어온다.
+
+// TODO 카테고리별 메뉴판 관리
+// - [ ] 에스프레소 메뉴판 관리
+// - [ ] 프라푸치노 메뉴판 관리
+// - [ ] 블렌디드 메뉴판 관리
+// - [ ] 티바나 메뉴판 관리
+// - [ ] 디저트 메뉴판 관리
+
+// TODO 페이지 접근시 최조 데이터 Read & Rendering
+// - [ ] 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다
+// - [ ] 에스프레소 메뉴를 페이지에 그려준다.
+
+//  TODO 품절 상태 관리
+//  - [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 `sold-out` class를 추가하여 상태를 변경한다.
+//  - [ ] 품절 버튼을 추가한다.
+//  - [ ] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+//  - [ ] 클릭이벤트에서 가장 가까운 li태그의  class속성 값에 sold-out을 추가한다
+
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+  setLocalStorage(menu) {
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },
+  getLocalStorage() {
+    localStorage.getItem("menu");
+  },
+};
+
 function App() {
+  // 상태는 변하는 데이터, 이앱에서 변하는 것이 무엇인가 - 메뉴명
+  this.menu = [];
   const updateMenuCount = () => {
     const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${menuCount}개`;
@@ -29,12 +69,14 @@ function App() {
       alert("값을 입력해 주세요");
       return;
     }
-    // if (e.key === "Enter") {
     const espressoMenuName = $("#espresso-menu-name").value;
-    const menuItemTemplate = (espressoMenuName) => {
-      return `
-          <li class="menu-list-item d-flex items-center py-2">
-          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+    this.menu.push({ name: espressoMenuName });
+    store.setLocalStorage(this.menu);
+    const template = this.menu
+      .map((menuitem, index) => {
+        return `
+          <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${menuitem.name}</span>
           <button
           type="button"
           class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -48,18 +90,20 @@ function App() {
           삭제
         </button>
       </li>`;
-    };
-    $("#espresso-menu-list").insertAdjacentHTML("beforeend", menuItemTemplate(espressoMenuName));
+      })
+      .join("");
 
+    $("#espresso-menu-list").innerHTML = template;
     updateMenuCount();
-
     $("#espresso-menu-name").value = "";
-    // }
   };
 
   const updateMenuName = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+    this.menu[menuId].name = updateMenuName;
+    store.setLocalStorage(this.menu);
     $menuName.innerText = updatedMenuName;
   };
 
@@ -93,4 +137,5 @@ function App() {
     addMenuName();
   });
 }
-App();
+
+const app = new App();
